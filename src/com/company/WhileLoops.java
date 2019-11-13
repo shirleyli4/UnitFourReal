448175bef3282bc6f7ebf04bedcb5b2431691dea
shuzhou li <shuzhou.li@ssfs.org>
2019-11-13 17:12:09 -0500

    from here to there, factors,pos neg counts

--- src/com/company/WhileLoops.java
+++ src/com/company/WhileLoops.java
@@ -1,12 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class WhileLoops {
     public static void main(String[] args) {
         int a =20;
         int b=30;
         System.out.println(fromHereToThere(a, b));
         System.out.println(facotrs(a));
-
+        System.out.println(countPosAndNed());
     }
 
     public static String fromHereToThere(int a, int b) {
@@ -27,7 +29,7 @@ public class WhileLoops {
         String list ="";
         while(num<=a){
             if(a%num==0){
-                list+=num;
+                list+=num+" ";
                 num++;
             }else{
                 num++;
@@ -35,5 +37,32 @@ public class WhileLoops {
         }return list;
     }
 
-
+    public static String countPosAndNed(){
+        Scanner input = new Scanner (System.in);
+        int posCount=0;
+        int negCount=0;
+        System.out.print("Enter a positive or negative number or 0 to quit: ");
+        int num = input.nextInt();
+        if(num>0){
+            posCount++;
+        }else if(num<0){
+            negCount++;
+        }
+        //if the first num is 0
+        if(num==0){
+            return "You entered 0. There were 0 positive and 0 negative numbers.\n";
+        }else {
+            //other regular situation
+            while (num != 0) {
+                System.out.print("Enter a positive or negative number or 0 to quit: ");
+                num = input.nextInt();
+                if (num > 0) {
+                    posCount++;
+                } else if (num < 0) {
+                    negCount++;
+                }
+            }
+        }
+        return "There were "+ posCount+" positive and "+negCount+" negative numbers.";
+    }
 }
