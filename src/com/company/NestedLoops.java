1e95116d3fec9aace591b05095ce0ee3b459fc22
shuzhou li <shuzhou.li@ssfs.org>
2019-12-03 09:01:00 -0500

    not finished

---
+++ src/com/company/NestedLoops.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class NestedLoops {
+    public static void main (String[] args){
+        Scanner input = new Scanner (System.in);
+        System.out.println(xSquare());
+        System.out.println("Enter n:");
+        int n=input.nextInt();
+        System.out.println(xSquare2(n));
+
+        System.out.println("Enter n:");
+        int n2=input.nextInt();
+        System.out.println(flippedTriangle(n2));
+
+        System.out.println(multiplicationTable());
+    }
+    public static String xSquare(){
+        String square ="";
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                square+="X";
+            }
+            square+="\n";
+        }
+        return square;
+    }
+
+    public static String xSquare2(int n){
+        String square="";
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                square+="X";
+            }
+            square+="\n";
+        }
+        return square;
+    }
+
+    public static String flippedTriangle (int n){
+        String tri="";
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=1;j--){
+                tri+="*";
+            }
+            tri+="\n";
+        }
+        return tri;
+    }
+
+    public static String multiplicationTable(){
+        String tab="";
+        for(int i=1;i<=9;i++){
+            for(int j=1;j<=9;j++){
+                tab+=i*j+"  ";
+            }
+            tab+="\n";
+        }
+        return tab;
+    }
+}
